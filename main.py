8fdd18217e718d38a9955b7848a9954229a8e1de
Aleksander Sushchenko <039pro@gmail.com>
2022-09-29 21:03:45 +0200

    Добавил метод к Лектору, но не работает, нужно разобаться в вводе данных

--- main.py
+++ main.py
@@ -1,33 +1,38 @@
-# Задание № 1. Наследование
 class Student:
-    def __init__(self, name, surname, gender):
+    def __init__(self, name, surname, gender_st):
         self.name = name
         self.surname = surname
-        self.gender = gender
+        self.gender_st = gender_st
         self.finished_courses = []
         self.courses_in_progress = []
-        self.grades = {}
-
+        self.grades_st = {}
 
 class Mentor:
-    def __init__(self, name, surname):
+    def __init__(self, name, surname,):
         self.name = name
         self.surname = surname
         self.courses_attached = []
+        self.grades_lec = {}
 
-    def rate_hw(self, student, course, grade):
+class Lecturer(Mentor):
+    def rate_lec(self, Lecturer, course, grade_lec):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
-            if course in student.grades:
-                student.grades[course] += [grade]
+            if course in student.grades_st:
+                student.grades_st[course] += [grade_st]
             else:
-                student.grades[course] = [grade]
+                student.grades_st[course] = [grade_st]
         else:
             return 'Ошибка'
 
-class Lecturer(Mentor):
-    pass
 class Reviewer(Mentor):
-    pass
+    def rate_hw(self, student, course, grade_st):
+        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+            if course in student.grades_st:
+                student.grades_st[course] += [grade_st]
+            else:
+                student.grades_st[course] = [grade_st]
+        else:
+            return 'Ошибка'
 
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
@@ -35,8 +40,11 @@ best_student.courses_in_progress += ['Python']
 cool_mentor = Mentor('Some', 'Buddy')
 cool_mentor.courses_attached += ['Python']
 
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
+cool_Reviewer = Reviewer ('Вася', 'Пупкин')
+cool_Reviewer.courses_attached += ['Python']
 
-print(best_student.grades)
+cool_Reviewer.rate_hw(best_student, 'Python', 10)
+cool_Reviewer.rate_hw(best_student, 'Python', 10)
+cool_Reviewer.rate_hw(best_student, 'Python', 10)
+
+print(best_student.grades_st)
