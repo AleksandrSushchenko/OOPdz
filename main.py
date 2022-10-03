1124a9ba093f7a968af71b2b02f4ca87614ab704
Aleksander Sushchenko <039pro@gmail.com>
2022-10-03 23:28:44 +0200

    Добавил метод __str__

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ class Student:
         self.zakonch_kurs = []
         self.kurs_v_processe = []
         self.ocenka = {}
-    def post_ocenku_lectoru (self, lector, kurs, ocenka_lectoru):
+
+    def post_ocenku_lectoru(self, lector, kurs, ocenka_lectoru):
         if isinstance(lector, Lecturer) and kurs in self.kurs_v_processe:
             if kurs in lector.ocenka_lectoru:
                 lector.ocenka_lectoru[kurs] += [ocenka_lectoru]
@@ -14,44 +15,64 @@ class Student:
                 lector.ocenka_lectoru[kurs] = [ocenka_lectoru]
         else:
             return "error"
+
+
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
         self.pricrepl_kurs = []
 
+
 class Lecturer(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
         self.ocenka_lectoru = {}
 
+    def sred_ocenka(self):
+        pass
+
+    def __str__(self):
+        return f'Имя:{self.name}\nФамилия:{self.surname}'
+
+
 class Reviewer (Mentor):
-    def post_ocenku_stud(self,student, kurs, ocenka):
+    def post_ocenku_stud(self, student, kurs, ocenka):
         if isinstance(student, Student) and kurs in self.pricrepl_kurs and kurs in student.kurs_v_processe:
             if kurs in student.ocenka:
                 student.ocenka[kurs] += [ocenka]
             else:
                 student.ocenka[kurs] = [ocenka]
         else:
-            return 'error'
+            return 'error' \
 
-student_1 = Student('Pety','Ivanov','M')
+    def __str__(self):
+        return f'Имя: {self.name}\nФамилия: {self.surname}'
+
+
+student_1 = Student('Петр', 'Иванов', 'М')
 student_1.kurs_v_processe += ['Phyton']
 
-lector_1 = Lecturer('Petr','Petrovich')
+lector_1 = Lecturer('Петр', 'Петрович')
 lector_1.pricrepl_kurs += ['Phyton']
 
-student_1.post_ocenku_lectoru(lector_1, 'Phyton', 3)
-student_1.post_ocenku_lectoru(lector_1, 'Phyton', 3)
-student_1.post_ocenku_lectoru(lector_1, 'Phyton', 3)
+prover_1 = Reviewer('Николай', 'Опарин')
+prover_1.pricrepl_kurs += ['Phyton']
 
-print(lector_1.ocenka_lectoru)
-
-prepod_1 = Reviewer('Taisa','Orlova')
+prepod_1 = Reviewer('Татьяна', 'Петровна')
 prepod_1.pricrepl_kurs += ['Phyton']
+
+student_1.post_ocenku_lectoru(lector_1, 'Phyton', 4)
+student_1.post_ocenku_lectoru(lector_1, 'Phyton', 3)
+student_1.post_ocenku_lectoru(lector_1, 'Phyton', 2)
 
 prepod_1.post_ocenku_stud(student_1, 'Phyton', 8)
 prepod_1.post_ocenku_stud(student_1, 'Phyton', 7)
 prepod_1.post_ocenku_stud(student_1, 'Phyton', 9)
 
+
 print(student_1.ocenka)
+print(lector_1.ocenka_lectoru)
+print(lector_1.sred_ocenka())
+print(prover_1)
+print(lector_1)
