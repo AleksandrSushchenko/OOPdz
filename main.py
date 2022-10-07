7e592ddde072893963e29f07d48c0a11d1efc54c
Aleksander Sushchenko <039pro@gmail.com>
2022-10-07 21:57:10 +0200

    End

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ class Student:
         self.zakonch_kurs = []
         self.kurs_v_processe = []
         self.ocenka = {}
+        self.sred_dz = []
 
     def post_ocenku_lectoru(self, lector, kurs, ocenka_lectoru):
         if isinstance(lector, Lecturer) and kurs in self.kurs_v_processe:
@@ -15,6 +16,23 @@ class Student:
                 lector.ocenka_lectoru[kurs] = [ocenka_lectoru]
         else:
             return "error"
+
+    def sred_ocenka_dz(self):
+        self.sred_dz = sum(self.ocenka['Phyton'])/len(self.ocenka['Phyton'])
+        return self.sred_dz
+
+    def __str__(self):
+        return f'Имя:{self.name}\n' \
+               f'Фамилия:{self.surname}\n' \
+               f'Средняя оценка за дз: {self.sred_ocenka_dz()}\n'\
+               f'Курсы в процессе изучения: {",".join(self.kurs_v_processe)}\n'\
+               f'Завершенные курсы: {",".join(self.zakonch_kurs)}\n'
+
+    def __lt__(self, other):
+        if not isinstance(other, Student):
+            print('Ошибка')
+            return
+        return student_1.sred_dz < student_2.sred_dz
 
 
 class Mentor:
@@ -28,12 +46,22 @@ class Lecturer(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
         self.ocenka_lectoru = {}
+        self.sred_ph = []
 
-    def sred_ocenka(self):
-        pass
+    def sred_ocenka_lect(self):
+        self.sred_ph = sum(self.ocenka_lectoru['Phyton'])/len(self.ocenka_lectoru['Phyton'])
+        return self.sred_ph
 
     def __str__(self):
-        return f'Имя:{self.name}\nФамилия:{self.surname}'
+        return f'Имя:{self.name}\n' \
+               f'Фамилия:{self.surname}\n' \
+               f'Средняя оценка за лекции: {self.sred_ocenka_lect()}\n'
+
+    def __lt__(self, other):
+        if not isinstance(other, Lecturer):
+            print('Ошибка')
+            return
+        return lector_1.sred_ph > lector_2.sred_ph
 
 
 class Reviewer (Mentor):
@@ -46,15 +74,27 @@ class Reviewer (Mentor):
         else:
             return 'error' \
 
+
     def __str__(self):
-        return f'Имя: {self.name}\nФамилия: {self.surname}'
+        return f'Имя: {self.name}\n' \
+               f'Фамилия: {self.surname}\n'
 
 
 student_1 = Student('Петр', 'Иванов', 'М')
 student_1.kurs_v_processe += ['Phyton']
+student_1.zakonch_kurs += ['Введение в програмирование']
 
-lector_1 = Lecturer('Петр', 'Петрович')
+student_2 = Student('Михайл', 'Михалыч', 'М')
+student_2.kurs_v_processe += ['Phyton']
+student_2.zakonch_kurs += ['Введение в програмирование']
+
+
+lector_1 = Lecturer('Алексей', 'Петрович')
 lector_1.pricrepl_kurs += ['Phyton']
+
+
+lector_2 = Lecturer('Ксенья', 'Петровна')
+lector_2.pricrepl_kurs += ['Phyton']
 
 prover_1 = Reviewer('Николай', 'Опарин')
 prover_1.pricrepl_kurs += ['Phyton']
@@ -62,17 +102,28 @@ prover_1.pricrepl_kurs += ['Phyton']
 prepod_1 = Reviewer('Татьяна', 'Петровна')
 prepod_1.pricrepl_kurs += ['Phyton']
 
-student_1.post_ocenku_lectoru(lector_1, 'Phyton', 4)
-student_1.post_ocenku_lectoru(lector_1, 'Phyton', 3)
-student_1.post_ocenku_lectoru(lector_1, 'Phyton', 2)
+student_1.post_ocenku_lectoru(lector_1, 'Phyton', 10)
+student_1.post_ocenku_lectoru(lector_1, 'Phyton', 10)
+student_1.post_ocenku_lectoru(lector_1, 'Phyton', 10)
+
+student_1.post_ocenku_lectoru(lector_2, 'Phyton', 9)
+student_1.post_ocenku_lectoru(lector_2, 'Phyton', 8)
+student_1.post_ocenku_lectoru(lector_2, 'Phyton', 7)
+
 
 prepod_1.post_ocenku_stud(student_1, 'Phyton', 8)
 prepod_1.post_ocenku_stud(student_1, 'Phyton', 7)
 prepod_1.post_ocenku_stud(student_1, 'Phyton', 9)
 
+prepod_1.post_ocenku_stud(student_2, 'Phyton', 10)
+prepod_1.post_ocenku_stud(student_2, 'Phyton', 10)
+prepod_1.post_ocenku_stud(student_2, 'Phyton', 10)
 
-print(student_1.ocenka)
-print(lector_1.ocenka_lectoru)
-print(lector_1.sred_ocenka())
+
 print(prover_1)
+print(student_1)
+print(student_2)
+print(student_1.sred_dz > student_2.sred_dz)
 print(lector_1)
+print(lector_2)
+print(lector_1.sred_ph > lector_2.sred_ph)
